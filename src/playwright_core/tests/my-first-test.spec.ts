3a2bbee5e46969396f2428d8753360d6d060bae4
dmoke <dmytromykhaliukk@gmail.com>
2024-01-31 13:58:44 +0200

    homework 3.2

---
+++ src/playwright_core/tests/my-first-test.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('check rozetka search', async ({ page }) => {
+    await page.goto('https://rozetka.com.ua/ua/');
+    await expect(page.getByPlaceholder('Я шукаю', { exact: true })).toBeVisible();
+})
+
+
+test('check rozetka burger menu', async ({ page }) => {
+    await page.goto('https://rozetka.com.ua/ua/');
+    await expect(page.getByLabel('Відкрити меню', { exact: true })).toBeVisible();
+})
+test('check rozetka catalog', async ({ page }) => {
+    await page.goto('https://rozetka.com.ua/ua/');
+    await expect(page.getByLabel('Каталог', { exact: true })).toBeVisible();
+})
+test('check rozetka cart button', async ({ page }) => {
+    await page.goto('https://rozetka.com.ua/ua/');
+    await expect(page.locator('rz-cart').getByRole('button', { exact: true })).toBeVisible();
+})
+test('check rozetka account button', async ({ page }) => {
+    await page.goto('https://rozetka.com.ua/ua/');
+    await expect(page.locator('rz-user').getByRole('button', { exact: true })).toBeVisible();
+})
